320d8890c8b7bc49065c9567e887e954a45d27aa
Guillermomajano <jmajano95@gmail.com>
2021-09-24 09:47:45 -0400

    middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,15 +2,17 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet", app.showSnippet)
 	mux.HandleFunc("/snippet/create", app.createSnippet)
 	mux.HandleFunc("/snippet/getall", app.showallT)
+
 	fileserver := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileserver))
 
-	return mux
+	return app.reciverPanic(app.logResquest(secureHeader(mux)))
+
 }
